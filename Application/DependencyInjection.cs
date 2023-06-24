3b36eda2f3062d89e0bed0cbcda16e728eb9072d
lazar.stojcevic <stojcevic00@gmail.com>
2023-06-24 12:33:02 +0200

    Set up code infrastructure

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        var assemby = typeof(DependencyInjection).Assembly;
+
+        services.AddMediatR(configuration =>
+            configuration
+            .RegisterServicesFromAssembly(assemby));
+
+        services.AddValidatorsFromAssembly(assemby);
+
+        return services;
+    }
+}
---
+++ Domain/User.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain;
+
+public class User : Entity
+{
+    public string Username { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+
+    private User(string username, string firstName, string lastName)
+    {
+        Username = username;
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    public static User Create(string username, string firstname, string lastname)
+    {
+        return new User(username, firstname, lastname);
+    }
+}
+
